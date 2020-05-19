3c1772f655e77064a13bfaddfa9c80b3c55fd663
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2020-05-19 07:57:05 +0200

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -44,3 +44,11 @@ function addcart() {
     alert("your Cart is Full, Buy Those Items First.");
   }
 }
+
+/* Products image */ 
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
